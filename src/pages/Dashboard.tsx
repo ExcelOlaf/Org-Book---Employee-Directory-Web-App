00f480943a2ef624074440599bb761ef1ba33df0
ExcelO0 <115517371+ExcelO0@users.noreply.github.com>
2026-02-15 21:43:53 -0600

    department connection frontend work

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { API_BASE_URL } from "../utils/apiRoute";
 import { useNavigate, useParams } from "react-router-dom";
 import { searchEmployees, type EmployeeData } from "../services/searchService";
